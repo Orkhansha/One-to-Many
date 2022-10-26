6852a8c12db4d56a13755aa7c4992aa746795147
Orkhansha <orkhansha@code.edu.az>
2022-10-27 02:41:16 +0400

    Added One to many files

---
+++ Fiorello-backend/EntityFrameworkProject/Migrations/20221026173038_SaveAllChanges.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkProject.Migrations
+{
+    public partial class SaveAllChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 26, 21, 30, 38, 236, DateTimeKind.Local).AddTicks(1379));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 26, 21, 30, 38, 236, DateTimeKind.Local).AddTicks(7165));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 26, 21, 30, 38, 236, DateTimeKind.Local).AddTicks(7199));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 13, 6, 5, 189, DateTimeKind.Local).AddTicks(6996));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 13, 6, 5, 191, DateTimeKind.Local).AddTicks(9941));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 13, 6, 5, 192, DateTimeKind.Local).AddTicks(136));
+        }
+    }
+}
---
+++ Fiorello-backend/EntityFrameworkProject/ViewModels/ProductViewModels/ProductDetailVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.ViewModels.ProductViewModels
+{
+    public class ProductDetailVM
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
